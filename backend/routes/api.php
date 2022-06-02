48c19f333be33dbf71dfb8e4e391c98e89a0bc89
Tamas Zentai <zentaitamas@gmail.com>
2022-06-02 16:18:18 +0100

    api is sending json data, frontend cleanup up

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,6 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    function csvtojson($file, $delimiter)
+    {
+        if (($handle = fopen($file, "r")) === false) {
+            die("can't open the file.");
+        }
+
+        $csv_headers = fgetcsv($handle, 4000, $delimiter);
+        $csv_json = array();
+
+        while ($row = fgetcsv($handle, 4000, $delimiter)) {
+            $csv_json[] = array_combine($csv_headers, $row);
+        }
+
+        fclose($handle);
+        return json_encode($csv_json);
+    }
+
+
+    $jsonresult = csvtojson("../interactions.csv", ",");
+    return $jsonresult;
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
