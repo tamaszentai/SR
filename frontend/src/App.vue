83a4ad9e983a8cc1b192c4c72da4bf8aefaa2846
Tamas Zentai <zentaitamas@gmail.com>
2022-06-02 16:24:24 +0100

    getting data from the backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <div id="app">
-Hello world
+  <div v-if="data">
+    {{ data }}
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-    }
+      data: null,
+    };
   },
-}
+  async mounted() {
+    const response = await axios.get("http://127.0.0.1:8000/api");
+    const data = response.data;
+    this.data = data;
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
